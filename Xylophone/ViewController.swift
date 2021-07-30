4bf6b25919d38a031017791e74ca1b88fd7249ef
Vamsi Pachamatla <vamsiraju835@gmail.com>
2021-07-30 12:34:46 +0530

    Initial Commit

---
+++ Xylophone/ViewController.swift
@@ -0,0 +1,33 @@
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        playSound()
+    }
+    
+    
+    func playSound() {
+        guard let path = Bundle.main.path(forResource: "beep", ofType:"mp3") else {
+            return }
+        let url = URL(fileURLWithPath: path)
+
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+}
+
